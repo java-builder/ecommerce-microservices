5788959070aa7aa066a60da13d6ddac711cdd150
lekhanhduc <lekhanhduccc@gmail.com>
2026-02-23 01:03:58 +0700

    Add global exception handling to `user-service` and enhance error response structure

--- user-service/src/main/java/com/javabuilder/userservice/exception/ErrorCode.java
+++ user-service/src/main/java/com/javabuilder/userservice/exception/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    INTERNAL_ERROR(500, "Unexpected error occurred while processing request in backend service", HttpStatus.INTERNAL_SERVER_ERROR),
     USER_ALREADY_EXISTS(400, "User already exists", HttpStatus.BAD_REQUEST)
 
     ;
---
+++ user-service/src/main/java/com/javabuilder/userservice/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.javabuilder.userservice.exception;
+
+import lombok.Builder;
+
+@Builder
+public record ErrorResponse(
+        int code,
+        String message,
+        String error,
+        String path,
+        long timestamp
+) {
+}
