a45fc06e420afd4c8d34d9da4f12a2012d01b43f
lekhanhduc <lekhanhduccc@gmail.com>
2026-03-01 21:09:54 +0700

    Initialize `service-discovery` project with Spring Boot, set up Eureka Server, define `.gitignore`, and configure dependencies and application properties. Update related services to integrate with Eureka.

--- api-gateway/src/main/java/com/javabuilder/apigateway/configuration/GatewayConfiguration.java
+++ api-gateway/src/main/java/com/javabuilder/apigateway/configuration/GatewayConfiguration.java
@@ -11,7 +11,7 @@ public class GatewayConfiguration {
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("user-service", r -> r.path("/api/v1/users/**", "/api/v1/auth/**").uri("http://localhost:8080"))
+                .route("user-service", r -> r.path("/api/v1/users/**", "/api/v1/auth/**").uri("lb://USER-SERVICE"))
                 .build();
     }
 }
