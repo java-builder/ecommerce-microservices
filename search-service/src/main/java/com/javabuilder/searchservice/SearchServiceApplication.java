26b305d53ef1e6403a0df3aeb09e7e299c6f617e
lekhanhduc <lekhanhduccc@gmail.com>
2026-03-05 15:48:13 +0700

    Initialize `search-service` project with Spring Boot, configure dependencies, add `.gitignore`, application properties, and integrate with Eureka for service discovery.

---
+++ search-service/src/main/java/com/javabuilder/searchservice/SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javabuilder.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SearchServiceApplication.class, args);
+    }
+
+}
