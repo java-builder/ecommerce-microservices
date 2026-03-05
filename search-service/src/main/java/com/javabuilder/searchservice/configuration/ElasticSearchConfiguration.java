85979855813db817ef955368d7fbfff89ee13b33
lekhanhduc <lekhanhduccc@gmail.com>
2026-03-05 17:20:54 +0700

    Update Elasticsearch integration in `search-service`:
    - Upgrade Elasticsearch Java client to version `9.3.2`.
    - Add `server-url` and `api-key` configurations for Elasticsearch.
    - Enhance `ElasticSearchConfiguration` to initialize the client with authentication.
    - Update README with API key details.

--- search-service/src/main/java/com/javabuilder/searchservice/configuration/ElasticSearchConfiguration.java
+++ search-service/src/main/java/com/javabuilder/searchservice/configuration/ElasticSearchConfiguration.java
@@ -1,22 +1,41 @@
 package com.javabuilder.searchservice.configuration;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import lombok.RequiredArgsConstructor;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
+import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
+import org.apache.hc.core5.http.Header;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.message.BasicHeader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import tools.jackson.databind.json.JsonMapper;
+import java.net.URISyntaxException;
 
 @Configuration
-@RequiredArgsConstructor
 public class ElasticSearchConfiguration {
 
+    @Value("${elasticsearch.server-url}")
     private String serverUrl;
+
+    @Value("${elasticsearch.api-key}")
     private String apiKey;
 
-    private final JsonMapper jsonMapper;
-
     @Bean
-    ElasticsearchClient elasticsearchClient() {
-        return null;
+    public ElasticsearchClient elasticsearchClient() throws URISyntaxException {
+        Rest5Client rest5Client = Rest5Client.builder(HttpHost.create(serverUrl))
+                .setDefaultHeaders(new Header[] {
+                    new BasicHeader("Authorization", "ApiKey " + apiKey),
+                })
+                .build();
+
+        Rest5ClientTransport transport = new Rest5ClientTransport(
+                rest5Client,
+                new JacksonJsonpMapper()
+        );
+
+        return new ElasticsearchClient(transport);
     }
+
 }
+
