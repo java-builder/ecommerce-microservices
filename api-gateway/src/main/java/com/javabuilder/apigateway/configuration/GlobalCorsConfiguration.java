4f2c70ea1c7926e34e1858dccf285d783e13d406
lekhanhduc <lekhanhduccc@gmail.com>
2026-09-23 21:44:42 +0700

    Add global CORS configuration to `api-gateway`:
    - Configure allowed origins, methods, headers, and credentials.
    - Introduce `CorsWebFilter` bean with URL-based configuration.

---
+++ api-gateway/src/main/java/com/javabuilder/apigateway/configuration/GlobalCorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.javabuilder.apigateway.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
+@Configuration
+public class GlobalCorsConfiguration {
+
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
+    }
+}
